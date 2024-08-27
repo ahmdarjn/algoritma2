f1c245da422cb6deb9fd33cc54831c89f5596534
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-27 12:38:50 +0700

    variabel pada php

---
+++ variabel pada php.php
@@ -0,0 +1,58 @@
+
+
+<?php
+
+$nisn1 = "Arjun";
+$harga = 1000;
+$nama_barang = "Kopi C++";
+$harga = 4000;
+$stok = 40;
+// membuat variabel baru
+$stok = 40;
+
+// mengisi ulang variabel dengan nilai baru
+$stok = 34;
+
+// Contoh: Salah
+// $!nama = "arjun";
+// $46rosi = "arjun slebew";
+// Contoh: Benar
+$nama = "Arjun";
+$rosi46 = "Arjun slebew";
+
+$nama_barang = "Handphone";
+$namaPembeli = "Arjun"; // menggunakan camelCase
+
+$nama_barang = "Handphone";
+$namaPembeli = "Sultan";
+$harga = 12000;
+
+$Belajar = "HTML";
+$BELAJAR = "CSS";
+$belajar = "JS";
+
+// ============================== cara mencetak ======================================
+echo 
+$nisn1,"<p>".
+$harga,"<p>".
+$nama_barang,"<p>".
+$harga,"<p>".
+$stok,"<p>".
+$nama,"<p>".
+$rosi46,"<p>".
+$nama_barang,"<p>".
+$namaPembeli,"<p>". 
+$nama_barang,"<p>".
+$namaPembeli,"<p>".
+$harga,"<p>".
+$Belajar,"<p>".
+$BELAJAR,"<p>".
+$belajar;
+
+
+
+
+
+
+
+
