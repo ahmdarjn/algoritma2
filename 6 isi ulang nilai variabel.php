4a75b8e6af9d503cd1651e39cb4c3a567446fe37
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-27 11:14:48 +0700

    isi ulang nilai variabel

---
+++ 6 isi ulang nilai variabel.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$stok1= 40;
+// membuat variabel baru
+$stok2 = 24;
+// mengisi ulang variabel dengan nilai baru
+$stok3 = 14;
+
+
+
+// =================== cara mencetak =======================
+echo 
+
+$stok1.",",
+$stok2.",",
+$stok3;
