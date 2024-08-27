c1421745325b9714930464385995ef423e8d893f
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-27 10:41:15 +0700

    SENSITIF

---
+++ 5 cara sensitif.php
@@ -0,0 +1,23 @@
+<?php
+
+$nama = "ahmad arjun trisula";
+$NAMA = "DARA";
+$umur = 15;
+const INI_KONSTANTA = 123;
+$ini_variabel = 23;
+$iniJugaVariabel = 49;
+$iniJugaVariabel;
+
+echo " ==== mencetak variabel ==== ";
+echo "<p>Nama saya adalah $nama";
+echo "<p>Nama seketaris adalah $NAMA";
+echo "<p>Umur saya adalah $umur";
+
+echo 
+
+    "<p>coba pakai blok program",
+    "<p>Nama saya adalah $nama",
+    "<p>Nama seketaris adalah $NAMA",
+    "<p>Umur saya adalah $umur";
+
+?>
